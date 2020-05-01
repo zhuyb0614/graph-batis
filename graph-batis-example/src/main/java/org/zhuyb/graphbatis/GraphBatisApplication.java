8450ba4f244096ee826138fe1819d90607174dd3
zhuyunbo <zhuyunbo@jianhuo.top>
2020-05-01 19:11:10 +0800

    误删

--- graph-batis-example/src/main/java/org/zhuyb/graphbatis/GraphBatisApplication.java
+++ graph-batis-example/src/main/java/org/zhuyb/graphbatis/GraphBatisApplication.java
@@ -1,11 +1,8 @@
 package org.zhuyb.graphbatis;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.zhuyb.graphbatis.interceptor.CleanSqlInterceptor;
 
 @SpringBootApplication
 @MapperScan(value = "org.zhuyb.graphbatis.mapper")
@@ -13,11 +10,6 @@ public class GraphBatisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GraphBatisApplication.class, args);
-    }
-
-    @Bean
-    ConfigurationCustomizer mybatisConfigurationCustomizer() {
-        return configuration -> configuration.addInterceptor(new CleanSqlInterceptor());
     }
 
 }
